c34262beaa003692670a9b53a92259c42617e65b
itthinx <itthinx@itthinx.com>
2026-03-27 17:53:32 +0100

    showing nonce and cookies

--- wp-acne.php
+++ wp-acne.php
@@ -22,11 +22,78 @@ if ( !defined( 'ABSPATH' ) ) {
 class WP_ACNE {
 
 	/**
-	 * Register actions and filters.
+	 * Register the init action.
+	 */
+	public static function boot() {
+		add_action( 'init', array( __CLASS__, 'init' ) );
+	}
+
+	/**
+	 * Register admin_head action and prepare admin notice.
 	 */
 	public static function init() {
 
+		add_action( 'admin_head', array( __CLASS__, 'admin_head' ) );
+
+		$cookies = array(
+			'USER_COOKIE' => USER_COOKIE,
+			'PASS_COOKIE' => PASS_COOKIE,
+			'AUTH_COOKIE' => AUTH_COOKIE,
+			'SECURE_AUTH_COOKIE' => SECURE_AUTH_COOKIE,
+			'LOGGED_IN_COOKIE' => LOGGED_IN_COOKIE,
+			'TEST_COOKIE' => TEST_COOKIE,
+			'RECOVERY_MODE_COOKIE' => RECOVERY_MODE_COOKIE
+		);
+
+		$entries = array();
+		foreach ( $cookies as $name => $id ) {
+			$value = $_COOKIE[$id] ?? null;
+			if ( $value !== null ) {
+				$entries[] =
+					sprintf(
+						'<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
+						esc_html( $name ),
+						esc_html( $id ),
+						esc_html( $value )
+					);
+			}
+		}
+
+		wp_admin_notice(
+			'<div class="wp-acne">' .
+			'<h2>WP ACNE | Admin Cookie Nonce Expose</h2>' .
+			'<h3>Nonce</h3>' .
+			'<p>_wpnonce ' . esc_html( wp_create_nonce() ) . '</p>' .
+			'<h3>Cookies</h3>' .
+			'<table>' .
+			'<thead>' .
+			'<tr>' .
+			'<th>Defined</th>' .
+			'<th>Constant</th>' .
+			'<th>Cookie</th>' .
+			'</tr>' .
+			'</thead>' .
+			'<tbody>' .
+			implode( '', $entries ) .
+			'</tbody>' .
+			'</table>' .
+			'</div>'
+		);
+	}
+
+	/**
+	 * Render styles.
+	 */
+	public static function admin_head() {
+		echo '<style type="text/css">' .
+			'.wp-acne { font-family: monospace; }' .
+			'.wp-acne tr { margin: 4px; }' .
+			'.wp-acne td { padding: 4px; }' .
+			'.wp-acne table thead tr th { border-bottom: 2px solid #eee; }' .
+			'.wp-acne table tbody tr td { border-right: 1px solid #ccc; border-radius: 0 0 4px 0; }' .
+			'.wp-acne table tbody tr td { border-bottom: 1px solid #ccc; border-radius: 0 0 4px 0; }' .
+			'</style>';
 	}
 }
 
-WP_ACNE::init();
+WP_ACNE::boot();
