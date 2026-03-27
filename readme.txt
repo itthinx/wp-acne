e6bf94965a35e57967a916deff24ff8ba7811650
itthinx <itthinx@itthinx.com>
2026-03-27 16:47:21 +0100

    initial

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== WP ACNE ===
+Contributors: itthinx, proaktion
+Donate link: https://www.itthinx.com/shop/
+Tags: cookie, nonce, debug, admin, tool
+Requires at least: 6.0
+Tested up to: 6.9
+Requires PHP: 7.4
+Stable tag: 1.0.0
+License: GPLv3
+
+WordPress Admin Cookie Nonce Expose. Debug tool. Developers Only. DO NOT use on Production Sites!
+
+== Description ==
+
+WordPress Admin Cookie Nonce Expose. Debug tool. Developers Only. DO NOT use on Production Sites!
---
+++ wp-acne.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: WP ACNE - Admin Cookie Nonce Expose
+ * Plugin URI: https://github.com/itthinx/xmlrpc-disable-extreme
+ * Description: WordPress Admin Cookie Nonce Expose. Debug tool. Developers Only. DO NOT use on Production Sites!
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Author: itthinx
+ * Author URI: https://www.itthinx.com
+ * Donate-Link: https://www.itthinx.com/shop/
+ * License: GPLv3
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Plugin class.
+ */
+class WP_ACNE {
+
+	/**
+	 * Register actions and filters.
+	 */
+	public static function init() {
+
+	}
+}
+
+WP_ACNE::init();
